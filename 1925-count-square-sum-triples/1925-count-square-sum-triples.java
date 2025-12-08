90551406105c6dff27e9a8379cb66ea51a24520c
Tanu-Sharma002 <sharma.tanu3004@gmail.com>
2025-12-08 11:09:39 +0530

    Time: 13 ms (56.16%), Space: 42 MB (49.69%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -5,17 +5,12 @@ class Solution {
             for (int b = 1; b <= n; b++) {
                 int sum = a*a + b*b;
 
-                if (helper(sum, n)) {
+                int c = (int)Math.sqrt(sum);
+                if (c * c == sum && c <= n) {
                     count++;
                 }
             }
         }
         return count;
-    }
-    static boolean helper(int sum, int n) {
-        for (int c = 1; c <= n; c++) {
-            if (c*c == sum) return true;
-        }
-        return false;
     }
 }
