12cf995e7086c6261a8f9374e03088c44ff1dd9a
Tanu-Sharma002 <sharma.tanu3004@gmail.com>
2025-12-27 21:34:08 +0530

    Time: 1 ms (100%), Space: 43.7 MB (7.27%) - LeetHub

---
+++ 3168-minimum-number-of-chairs-in-a-waiting-room/3168-minimum-number-of-chairs-in-a-waiting-room.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumChairs(String s) {
+        int count=0;
+        int max=0;
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch=='E'){
+                count++;
+            } else count--;
+            max=Math.max(count,max);
+        }
+        return max;
+    }
+}
