512834199f0a3420396f204249e2bcc2b4a91a8d
Tanu-Sharma002 <sharma.tanu3004@gmail.com>
2026-01-11 12:32:59 +0530

    Time: 0 ms (100%), Space: 48.6 MB (40.23%) - LeetHub

---
+++ 0598-range-addition-ii/0598-range-addition-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxCount(int m, int n, int[][] ops) {
+        if(ops.length==0) return m*n;
+        int row=m;
+        int col=n;
+        for(int[] op: ops){
+            row=Math.min(row,op[0]);
+            col=Math.min(col,op[1]);
+        }
+        return row*col;
+    }
+}
