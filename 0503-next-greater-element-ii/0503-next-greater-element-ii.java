afb01d3a3ac200b07ad5756511b09a6d5c52f8ed
Tanu-Sharma002 <sharma.tanu3004@gmail.com>
2026-03-05 23:14:42 +0530

    Time: 3 ms (99.2%), Space: 48.4 MB (66.48%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        Arrays.fill(result, -1); 
+        Deque<Integer> stack = new ArrayDeque<>(); 
+        for (int i = 0; i < n * 2; i++) {
+            int num = nums[i % n];
+            while (!stack.isEmpty() && nums[stack.peek()]<num) {
+                result[stack.pop()] = num;
+            }
+            if (i < n) {
+                stack.push(i);
+            }
+        }
+        return result;
+    }
+}
