9e5cc8630415ad7e7494baa4ef0149def4b69684
Tanu-Sharma002 <sharma.tanu3004@gmail.com>
2026-03-24 22:46:31 +0530

    Time: 3 ms (53.58%), Space: 48.7 MB (35.97%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        Map<Integer, List<String>> backtrack = new HashMap<>();
+        return helper(s, 0, set, backtrack);
+    }
+    private List<String> helper(String s, int start, Set<String> set, Map<Integer, List<String>> backtrack){
+        if(backtrack.containsKey(start)) return backtrack.get(start);
+        List<String> result = new ArrayList<>();
+        if(start==s.length()){
+            result.add("");
+            return result;
+        }
+        for(int i=start+1; i<=s.length(); i++){
+            String word=s.substring(start,i);
+            if(set.contains(word)){
+                List<String> next=helper(s,i,set,backtrack);
+                for(String sub:next){
+                    if(sub.isEmpty()) result.add(word);
+                    else result.add(word + " " + sub);
+                }
+            }
+        }
+        backtrack.put(start,result);
+        return result;
+    }
+}
