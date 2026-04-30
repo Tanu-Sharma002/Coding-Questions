ac1d7b919bed527029d2981bff8ba52a6e761d52
Tanu-Sharma002 <sharma.tanu3004@gmail.com>
2026-04-30 10:02:01 +0530

    Time: 45 ms (25.19%), Space: 46.8 MB (83.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,6 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
+        /*
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -10,6 +11,15 @@ public class Solution {
             }
 
             map.put(nums[i], i);
+        }
+        return new int[0];
+        */
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
         }
         return new int[0];
     }
