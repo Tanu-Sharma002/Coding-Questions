648de31e9291b4a896ecb557e57b64dd7e2c82f8
Tanu-Sharma002 <sharma.tanu3004@gmail.com>
2026-05-08 01:01:58 +0530

    Time: 7 ms (32.26%), Space: 47.3 MB (86.99%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -3,7 +3,7 @@ class Solution {
         int max=0;
         for(int i=0; i<n.length(); i++){
             char ch=n.charAt(i);
-            int a = ch-'0';
+            int a = ch-'0';  /// isse string integer mein convert hota hai
             max=Math.max(max,a);
         }
         return max;
