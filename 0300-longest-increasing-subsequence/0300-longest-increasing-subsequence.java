6c7b809fdb3d6bdf51fd0238523fcbeade46fbd9
Tanu-Sharma002 <sharma.tanu3004@gmail.com>
2026-05-15 10:43:22 +0530

    Time: 82 ms (21.98%), Space: 46.2 MB (40.93%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,3 +1,5 @@
+/* 
+// Through memoization
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
@@ -14,3 +16,28 @@ class Solution {
         return max;
     }
 }                       
+
+*/
+
+// recursive approach with memoization
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int ans=0;
+        int dp[] = new int[2501];
+        Arrays.fill(dp,-1);
+        for(int i=0; i<nums.length; i++){
+            ans = Math.max(ans,helper(nums,i,dp));
+        }
+        return ans;
+    }
+    public static int helper(int arr[], int i, int dp[]){
+        if(dp[i] != -1) return dp[i];
+        int ans=1;
+        for(int j=0; j<i; j++){
+            if(arr[i]>arr[j]) ans=Math.max(ans,helper(arr,j,dp)+1);
+        }
+        return dp[i]=ans;
+    }
+}
+
+//
