a7ca6a4426fd2a1e0a900506efbb37104780c71a
Tanu-Sharma002 <sharma.tanu3004@gmail.com>
2026-05-19 21:29:41 +0530

    Time: 2718 ms (5.01%), Space: 83.1 MB (58.81%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        int ans[] = new int[nums.length];
+        if (nums == null || nums.length == 0) return ans;
+        quickSort(nums, 0, nums.length-1);
+        for (int i : nums) res.add(i);
+        for(int i=0; i<res.size(); i++){
+            ans[i]=res.get(i);
+        }
+        return ans;
+    }
+    private void quickSort(int[] nums, int l, int r) {
+        if (l >= r) return;
+        int mid = partition(nums, l, r);
+        quickSort(nums, l, mid);
+        quickSort(nums, mid + 1, r);
+    }
+    private int partition(int[] nums, int l, int r) {
+        int pivot = nums[l];
+        while (l < r) {
+            while (l < r && nums[r] >= pivot) r--;
+            nums[l] = nums[r];
+            while (l < r && nums[l] <= pivot) l++;
+            nums[r] = nums[l];
+        }
+        nums[l] = pivot;
+        return l;
+    }
+}
