9a3eb1bdc92150baf53fc32de4338f937224046a
Tanu-Sharma002 <sharma.tanu3004@gmail.com>
2026-07-22 12:26:23 +0530

    Time: 1 ms (80.28%), Space: 43 MB (64.23%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n=s.length();
+        int dp[] = new int[n+1];
+        if(s.length()==0 || s.charAt(0)=='0') return 0;
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2; i<=s.length(); i++){
+            int first=s.charAt(i-1)-'0';
+            int second=Integer.parseInt(s.substring(i-2,i));
+            if(first != 0) dp[i]+=dp[i-1];
+            if(10<=second && second<=26) dp[i]+=dp[i-2];
+        }
+        return dp[n];
+    }
+}
